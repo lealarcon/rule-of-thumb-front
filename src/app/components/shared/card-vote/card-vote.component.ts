004b42ce24ef31eff58094595ec72deecbb68e38
Luis <lealarcon@devneedzaio.com>
2022-03-31 09:41:59 -0500

    CSS in vote component  for desktop, services and model included to get data from API

--- src/app/components/shared/card-vote/card-vote.component.ts
+++ src/app/components/shared/card-vote/card-vote.component.ts
@@ -1,36 +1,47 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { Vote } from 'src/app/models/class/votes';
+var moment = require('moment');
+moment().format();
 @Component({
   selector: 'app-card-vote',
   templateUrl: './card-vote.component.html',
   styleUrls: ['./card-vote.component.sass']
 })
 export class CardVoteComponent implements OnInit {
-  @Input() data: any;
+  @Input() data: Vote;
 
-  public thumbs={
-    up:0,
-    down:0
+  public thumbs = {
+    up: 0,
+    down: 0,
+    selectedUp: false,
+    selectedDown: false
   }
+
+  public totalVotes = 0;
+  public timeAgo = moment();
   constructor() { }
-  /*
-   {
-            "name": "Kanye West",
-            "description": "Born in Atlanta and raised in Chicago, West was first known as a producer for Roc-A-Fella Records in the early 2000s, producing singles for several mainstream artists.",
-            "category": "entertainment",
-            "picture": "kanye.png",
-            "lastUpdated": "2020-03-10T23:08:57.892Z",
-            "votes": {
-                "positive": 23,
-                "negative": 36
-            }
-        },
-         */
+  
   ngOnInit(): void {
 
-    let totalVotes= this.data.votes.positive + this.data.votes.negative
-    this.thumbs.up = Number((this.data.votes.positive * 100 / totalVotes).toFixed(0))
-    this.thumbs.down = Number((this.data.votes.negative * 100 / totalVotes).toFixed(0))
+    this.totalVotes = this.data.votes.positive + this.data.votes.negative
+    this.thumbs.up = Number((this.data.votes.positive * 100 / this.totalVotes).toFixed(0))
+    this.thumbs.down = Number((this.data.votes.negative * 100 / this.totalVotes).toFixed(0))
+
+    var a = moment();
+    var b = moment(this.data.lastUpdated);
+    this.timeAgo = a.from(b, true)
+
   }
 
+  thumbSelected(type: string): void {
+    if (type == 'up') {
+      if (this.thumbs.selectedUp == true) { this.thumbs.selectedUp = false; this.thumbs.selectedDown = false }
+      else { this.thumbs.selectedUp = true; this.thumbs.selectedDown = false }
+    }
+    if (type == 'down') {
+      if (this.thumbs.selectedDown == true) { this.thumbs.selectedUp = false; this.thumbs.selectedDown = false }
+      else { this.thumbs.selectedDown = true; this.thumbs.selectedUp = false }
+    }
+
+  }
 }
